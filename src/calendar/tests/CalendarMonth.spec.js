4e13aed6d9f8f281305dad4552b4105a6b1d8e21
Nicolas <nicolas.senechal@gmail.com>
2015-08-25 16:06:38 +0100

    Testing skeleton for CalandarMonth

--- src/calendar/tests/CalendarMonth.spec.js
+++ src/calendar/tests/CalendarMonth.spec.js
@@ -1,25 +1,106 @@
 import React from 'react/addons';
-import CalendarDatePeriod from '../CalendarDatePeriod.jsx';
+import CalendarMonth from '../CalendarMonth.jsx';
+import CalendarDate from '../CalendarDate.jsx';
+import moment from 'moment';
+import {} from 'moment-range';
+
 
 const TestUtils = React.addons.TestUtils;
 
-describe('The CalendarDatePeriod Component', function () {
+describe('The CalendarMonth Component', function () {
   beforeEach(() => {
-    this.spyCx = spyOn(CalendarDatePeriod.prototype.__reactAutoBindMap, 'cx').and.returnValue('my-class');
+    this.spyCx = spyOn(CalendarMonth.prototype.__reactAutoBindMap, 'cx').and.returnValue('my-class');
 
     var shallowRenderer = TestUtils.createRenderer();
-    shallowRenderer.render(<CalendarDatePeriod period='month' color='pink'/>);
+    var firstOfWeek = 0;
+    var firstOfMonth = moment();
+    shallowRenderer.render(<CalendarMonth
+      firstOfWeek={firstOfWeek}
+      firstOfMonth={firstOfMonth}
+      enabledRange={moment.range()}
+      dateComponent={CalendarDate}
+    />);
     this.renderedComponent = shallowRenderer.getRenderOutput();
   });
 
   it('should render the right element', () => {
     expect(this.renderedComponent.type).toBe('div');
     expect(this.spyCx).toHaveBeenCalledWith({
-      modifiers: {
-        month: true,
-      },
+      element: 'Month',
     });
     expect(this.renderedComponent.props.className).toEqual('my-class');
-    expect(this.renderedComponent.props.style).toEqual({ backgroundColor: 'pink' });
+  });
+
+  describe('has a logical header', () => {
+
+    it('which is a div with the correct class', () => {
+
+    });
+
+    describe('displaying month information', () => {
+
+      it('which displays the name of the month', () => {
+
+      });
+
+      it('which does not show navigation if props.disableNavigation is true', () => {
+
+      });
+
+      it('which shows navigation if props.disableNavigation is false', () => {
+
+      });
+
+      it('which calls props.onMonthChange if props.disableNavigation is false and if the selected value changes', () => {
+
+      });
+
+    });
+
+    describe('displaying year information', () => {
+
+      it('which displays the name of the year', () => {
+
+      });
+
+      it('which does not show navigation if props.disableNavigation is true', () => {
+
+      });
+
+      it('which shows navigation if props.disableNavigation is false', () => {
+
+      });
+
+      it('which calls props.onYearChange if props.disableNavigation is false and if the selected value changes', () => {
+
+      });
+
+    });
+
+    describe('has a table', () => {
+
+      it('which has the expected className', () => {
+
+      });
+
+      describe('whose head contains day information', () => {
+
+        it('has the correct type and class', () => {
+
+        });
+
+        it('contains the expected elements', () => {
+
+        });
+
+      });
+
+    });
+
+    it('has a body containing the weeks', () =>{
+
+    });
+
+
   });
 });
