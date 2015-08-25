d10fe0cffc881a9fb316def1d41dcca011c18ce2
Nicolas <nicolas.senechal@gmail.com>
2015-08-25 12:27:40 +0100

    Basic setup for CalendarMonth

---
+++ src/calendar/tests/CalendarMonth.spec.js
@@ -0,0 +1,25 @@
+import React from 'react/addons';
+import CalendarDatePeriod from '../CalendarDatePeriod.jsx';
+
+const TestUtils = React.addons.TestUtils;
+
+describe('The CalendarDatePeriod Component', function () {
+  beforeEach(() => {
+    this.spyCx = spyOn(CalendarDatePeriod.prototype.__reactAutoBindMap, 'cx').and.returnValue('my-class');
+
+    var shallowRenderer = TestUtils.createRenderer();
+    shallowRenderer.render(<CalendarDatePeriod period='month' color='pink'/>);
+    this.renderedComponent = shallowRenderer.getRenderOutput();
+  });
+
+  it('should render the right element', () => {
+    expect(this.renderedComponent.type).toBe('div');
+    expect(this.spyCx).toHaveBeenCalledWith({
+      modifiers: {
+        month: true,
+      },
+    });
+    expect(this.renderedComponent.props.className).toEqual('my-class');
+    expect(this.renderedComponent.props.style).toEqual({ backgroundColor: 'pink' });
+  });
+});
