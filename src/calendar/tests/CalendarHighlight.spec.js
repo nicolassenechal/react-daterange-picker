6bd3cbe8fae9b7876ff7085376a9d48dc8d9a006
Nicolas <nicolas.senechal@gmail.com>
2015-08-25 11:19:41 +0100

    Linted specs

--- src/calendar/tests/CalendarHighlight.spec.js
+++ src/calendar/tests/CalendarHighlight.spec.js
@@ -7,23 +7,23 @@ import BemMixin from '../../utils/BemMixin';
 const TestUtils = React.addons.TestUtils;
 
 describe('The CalendarHighlight Component', function () {
-    beforeEach(() => {
-        this.spyCx = spyOn(CalendarHighlight.prototype.__reactAutoBindMap, 'cx').and.returnValue('my-class');
+  beforeEach(() => {
+    this.spyCx = spyOn(CalendarHighlight.prototype.__reactAutoBindMap, 'cx').and.returnValue('my-class');
 
-        var shallowRenderer = TestUtils.createRenderer();
-        shallowRenderer.render(<CalendarHighlight pending={true} modifier='test'/>);
-        this.renderedComponent = shallowRenderer.getRenderOutput();
-    });
+    var shallowRenderer = TestUtils.createRenderer();
+    shallowRenderer.render(<CalendarHighlight pending={true} modifier='test'/>);
+    this.renderedComponent = shallowRenderer.getRenderOutput();
+  });
 
-    it('should render the right element', () => {
-        expect(this.renderedComponent.type).toBe('div');
-        expect(this.spyCx).toHaveBeenCalledWith({
-            states: {
-            },
-            modifiers: {
-                test: true
-            },
-        });
-        expect(this.renderedComponent.props.className).toEqual('my-class');
+  it('should render the right element', () => {
+    expect(this.renderedComponent.type).toBe('div');
+    expect(this.spyCx).toHaveBeenCalledWith({
+      states: {
+      },
+      modifiers: {
+        test: true,
+      },
     });
+    expect(this.renderedComponent.props.className).toEqual('my-class');
+  });
 });
